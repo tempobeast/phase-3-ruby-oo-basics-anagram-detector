2cb16576cbea4c9158241fce6905426df1a4ad76
Zach <shaw.zacht@gmail.com>
2022-05-15 14:38:02 -0700

    commit tree 9ff4c246da041d0b41b7362631d69fc35d40aaea

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+
+
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        match_words = []
+        split_words = array.map do |word|
+        
+        @word.chars.sort == word.chars.sort ? match_words.push(word) : nil
+        end
+        match_words
+    end
+
+end
